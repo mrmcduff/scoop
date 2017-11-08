af099a6c3045f901162eb71ec5a59ec06188aa1f
Michael McDuffee <mrmcduff@gmail.com>
2017-11-07 20:54:50 -0800

    creating daggerable project for sb

---
+++ scoop-basics-d2/src/main/java/com/rotoai/scoop_basics_d2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rotoai.scoop_basics_d2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crumbsandcoffee.scoop_basics_d2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
